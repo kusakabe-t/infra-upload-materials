a8cac2b6fb2ddc3dc7844e11c47c09ffa8077777
pilefort <pilefort2020@gmail.com>
2022-01-09 16:15:15 +0900

    add sample api

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api, format: 'json' do
+    resources :test, controller: :test
+  end
 end
