fd4f0c57b51d60a0380a9d75d1cba1c0e7d2f206
pilefort <pilefort2020@gmail.com>
2022-01-09 15:43:18 +0900

    remove api sample

--- api-sample/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  namespace :api, format: 'json' do
-    resources :test, controller: :test
-  end
-end
