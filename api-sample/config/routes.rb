5de91571162c027cf4529089eadc76542d22b739
pilefort <pilefort2020@gmail.com>
2022-01-09 15:29:40 +0900

    add api test

---
+++ api-sample/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  namespace :api, format: 'json' do
+    resources :test, controller: :test
+  end
+end
